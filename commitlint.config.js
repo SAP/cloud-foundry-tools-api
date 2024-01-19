d61ab4acf4bd9cc12d1f5b9c7aaef63214c022d7
Rima Sirich <rima.sirich@sap.com>
2024-01-19 10:16:15 +0200

    fix: commitlint.config.js ignores pattern

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  ignores: [(message) => /^chore: bump \[.+]\(.+\) from .+ to .+\.$/m.test(message)],
+  ignores: [(message) => message.includes('chore: bump')],
 }
